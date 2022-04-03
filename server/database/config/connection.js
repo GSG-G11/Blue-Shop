d7da21d699e4b616f398e376c5c8dee09319ce19
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-04-03 14:24:50 +0300

    create database connenction and build sql and insert fake products Relates #5,6,9

---
+++ server/database/config/connection.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+require('env2')('.env');
+
+const {
+  NODE_ENV, DEV_DB_URL, PROD_DB_URL,
+} = process.env;
+
+let dbURl = PROD_DB_URL;
+let ssl = false;
+
+if (NODE_ENV === 'dev') {
+  dbURl = DEV_DB_URL;
+} else if (NODE_ENV === 'start') {
+  ssl = { rejectUnauthenticated: false };
+}
+
+const connection = new Pool({
+  connectionString: dbURl,
+  ssl,
+});
+
+module.exports = connection;
