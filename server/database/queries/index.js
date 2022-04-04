ae6ba630b8e20512f7e24a3de9c7e56ea3e8963b
SalmaIssa96 <salmaissa196@gmail.com>
2022-04-05 01:28:02 +0300

    done with update query and controller relates #10 , #16

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,5 +1,8 @@
 const addProductQuery = require('./addProductQuery');
 const getAllProductsQuery = require('./getAllProducts');
 const deleteProductQuery = require('./deleteProductQuery');
+const updateProductQuery = require('./editProductQuery');
 
-module.exports = { addProductQuery, deleteProductQuery, getAllProductsQuery };
+module.exports = {
+  addProductQuery, deleteProductQuery, getAllProductsQuery, updateProductQuery,
+};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const { getAllProducts, addProduct, deleteProduct } = require('../controllers');
+const {
+  getAllProducts, addProduct, deleteProduct, updateProduct,
+} = require('../controllers');
 
 const apiRouter = express.Router();
-
 apiRouter.get('/products', getAllProducts);
 apiRouter.post('/product', addProduct);
 apiRouter.delete('/products/:id', deleteProduct);
+apiRouter.put('/products/:id', updateProduct);
 module.exports = apiRouter;
