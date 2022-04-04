44de82f8f5c81660a629cfa2bf0f52c18e9a01f4
unknown <stm1998@hotmail.com>
2022-04-04 12:31:21 +0300

    create navbar component and landing page Realtes #21

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Card from './component/cards/Card';
-import Category from './component/Category';
-import Price from './component/Price';
+
+import Nav from './component/Nav';
+import Landing from './component/Landing';
+
 class App extends Component {
   state = {
     max: 9.99,
     min: 2.2,
     category: 'mens clothing',
+    logged:true,
   };
 
   onSetValue = (e) => {
@@ -21,15 +24,20 @@ class App extends Component {
   };
 
   render() {
+    const actions = { setCategory:this.setCategory, onSetValue:this.onSetValue }
+    
     return (
-      <div className="App">
-        <Category setCategory={this.setCategory} />
-        <Price onSet={this.onSetValue} />
-        <Card
-          min={this.state.min}
-          max={this.state.max}
-          category={this.state.category}
-        />
+      <div className='App'>
+        <Router>
+          <Nav logged={this.state.logged} />
+          <Routes>
+            <Route path='/' element={<Landing actions={actions} values={this.state} />} />
+            <Route path='/login' element={<h1>Login</h1>} />
+            <Route path='/cart' element={<h1>Cart</h1>} />
+            <Route path='/product/:id' element={<h1>Product</h1>} />
+            <Route path='/add-product' element={<h1>Add product</h1>} />
+          </Routes>
+        </Router>
       </div>
     );
   }
