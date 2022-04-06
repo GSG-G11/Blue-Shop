2bf77e6e9352b9a02630a93bfd27c1a1dfa8fe51
abdulrahman-2020 <akalousa@students.iugaza.edu.ps>
2022-04-06 19:43:04 +0300

    style for edit form Relates #63

--- client/src/component/editProduct/EditProduct.js
+++ client/src/component/editProduct/EditProduct.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
-import axios from 'axios';
-import './editProduct.css';
+import React, { Fragment } from "react";
+import axios from "axios";
+import "./editProduct.css";
 class UpdateProduct extends React.Component {
   state = {
-    name: '',
-    price: '',
-    description: '',
-    image: '',
-    category: 'mens clothing',
-    successMsg: '',
+    name: "",
+    price: "",
+    description: "",
+    image: "",
+    category: "mens clothing",
+    successMsg: "",
   };
 
   handleChange = ({ target }) => {
@@ -34,54 +34,70 @@ class UpdateProduct extends React.Component {
       <Fragment>
         <div className="update-container">
           <form className="update-form" onSubmit={this.editProduct}>
-            <label>
-              Name:
-              <input
-                type="text"
-                placeholder={this.props.element.name}
-                onChange={this.handleChange}
-                name="name"
-              />
-            </label>
-
-            <label>
-              Price:
-              <input
-                type="number"
-                placeholder={this.props.element.price}
-                onChange={this.handleChange}
-                name="price"
-              />
-            </label>
-
-            <label>
-              Description:
-              <input
-                type="text"
-                placeholder={this.props.element.description}
-                onChange={this.handleChange}
-                name="description"
-              />
-            </label>
-
-            <label>
-              image:
-              <input
-                type="text"
-                placeholder={this.props.element.image}
-                onChange={this.handleChange}
-                name="image"
-              />
-            </label>
-
-            <select name="category" onChange={this.handleChange}>
-              <option value="mens clothing">mens clothing</option>
-              <option value="jewelry">jewelry</option>
-              <option value="electronics">electronics</option>
-              <option value="women clothing">women clothing</option>
+            <div className="input-label-div">
+              <label>
+                Name:
+                <input
+                  type="text"
+                  placeholder={this.props.element.name}
+                  onChange={this.handleChange}
+                  name="name"
+                />
+              </label>
+            </div>
+            <div className="input-label-div">
+              <label>
+                Price:
+                <input
+                  type="number"
+                  placeholder={this.props.element.price}
+                  onChange={this.handleChange}
+                  name="price"
+                />
+              </label>
+            </div>
+            <div className="input-label-div">
+              <label>
+                Description:
+                <input
+                  type="text"
+                  placeholder={this.props.element.description}
+                  onChange={this.handleChange}
+                  name="description"
+                />
+              </label>
+            </div>
+            <div className="input-label-div">
+              <label>
+                image:
+                <input
+                  type="text"
+                  placeholder={this.props.element.image}
+                  onChange={this.handleChange}
+                  name="image"
+                />
+              </label>
+            </div>
+            <select
+              className="select-category"
+              name="category"
+              onChange={this.handleChange}
+            >
+              <option className="category-option" value="mens clothing">
+                mens clothing
+              </option>
+              <option className="category-option" value="jewelry">
+                jewelry
+              </option>
+              <option className="category-option" value="electronics">
+                electronics
+              </option>
+              <option className="category-option" value="women clothing">
+                women clothing
+              </option>
             </select>
             <button className="add-btn" type="submit">
-              {' '}
+              {" "}
               submit
             </button>
           </form>
